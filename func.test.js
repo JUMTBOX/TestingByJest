23dc338c88ca16df951ef6dfc70b3562fc8bc075
JUMTBOX <yjey12@naver.com>
2023-05-03 22:49:29 +0900

    ..

--- func.test.js
+++ func.test.js
@@ -8,3 +8,8 @@ test("변수의 얕은 비교는 테스가 되나요?", () => {
   const str = "테스트다";
   expect(str + " 가능?").toBe("테스트다 가능?");
 });
+
+test("테스트 연습", () => {
+  const str = "연습이다";
+  expect(str).toBe("연습이다");
+});
