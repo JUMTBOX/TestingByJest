4b0f0734028871e1f3876025b7a3045ac797b977
JUMTBOX <yjey12@naver.com>
2023-05-04 22:01:23 +0900

    ...

--- err.test.ts
+++ err.test.ts
@@ -5,3 +5,7 @@ const throwError = (): never => {
 test("에러가 발생하는지 테스트", () => {
   expect(() => throwError()).toThrow();
 });
+
+test("에러가 발생하는지 테스트", () => {
+  expect(() => throwError()).toThrow();
+});
